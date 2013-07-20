9a369a55cabc079efdbd14b45811f0b1f261c60d
liuxin04 <liuxin04@ubuntu.(none)>
2013-07-20 10:07:01 -0700

    transfor google svn

---
+++ src/main/java/org/uranus/Ut.java
@@ -0,0 +1,13 @@
+package org.uranus;
+
+import org.apache.log4j.Logger;
+
+public class Ut
+{
+	private static Logger logger = Logger.getLogger(Ut.class);
+
+    public static void main( String[] args )
+    {
+		logger.info("Uranus UnitTest");
+    }
+}
